c6e6c694e61974f9a6049d57c6e94e6dcd90a55c
thienhdbse170185 <thienhdbse170185@fpt.edu.vn>
2024-10-11 16:09:15 +0700

    FJEXE-12-ui-design-implementation-register-passenger
    divide to setting-bottom-sheet component to reuse

---
+++ lib/core/common/widgets/settings_bottom_sheet.dart
@@ -0,0 +1,48 @@
+import 'package:f_journey/core/common/cubits/theme_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void showSettingsBottomSheet(BuildContext context) {
+  showModalBottomSheet(
+    context: context,
+    builder: (BuildContext context) {
+      return Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Dark Mode',
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                Switch(
+                  value: context.watch<ThemeCubit>().state,
+                  onChanged: (bool value) {
+                    // Directly toggle the theme from the Cubit
+                    context.read<ThemeCubit>().toggleTheme();
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(height: 16),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: FilledButton(
+                style: FilledButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.secondary,
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop(); // Close the bottom sheet
+                },
+                child: const Text('Close'),
+              ),
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}
